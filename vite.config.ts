072d8aa27c58175b79b8f9ce12f5a93af4a342eb
bilal-hos <bilalfjk@gmail.com>
2026-05-22 18:31:46 +0300

    klab

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 5177,
     proxy: {
       "/api": {
         target: "https://api.foodlens.cloud:4000",
